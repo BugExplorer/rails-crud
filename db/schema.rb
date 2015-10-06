a8b279cfe20f5ae5d293dedbc471998782933371
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2015-10-06 09:56:54 +0300

    Add tags_ids to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004124911) do
+ActiveRecord::Schema.define(version: 20151006065206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,14 +45,18 @@ ActiveRecord::Schema.define(version: 20151004124911) do
     t.datetime "updated_at",       null: false
   end
 
-  add_index "comments", ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id", using: :btree
+  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id", using: :btree
+  add_index "comments", ["commentable_type"], name: "index_comments_on_commentable_type", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
     t.string   "author_name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "tag_ids",     default: [],              array: true
   end
+
+  add_index "posts", ["tag_ids"], name: "index_posts_on_tag_ids", using: :gin
 
 end

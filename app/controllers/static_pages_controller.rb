33105f58c3ee08b2754df96e11fd7c95d3ba6491
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2015-10-13 10:13:56 +0300

    Optimize tag cloud

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,14 +3,20 @@ class StaticPagesController < ApplicationController
 
   def home
     @entries = [] # Array of hashes
+    @tags_hash = Hash.new(0)
+    @content = ContentView.all
+    @content.each do |content|
+      content.tag_ids.each do |tag|
+          @tags_hash[tag] +=1
+      end
+    end
+
     @tags.each do |tag|
-      # Get entries count of the current tag
-      tag_entries_count = ContentView.any_tags(tag.id).count
       # Get current tag's font size
       _class =
-        case tag_entries_count
-        when 0..15 then 'sm'
-        when 16..20 then 'nm'
+        case @tags_hash[tag.id]
+        when 0..(@content.count / 8) then 'sm'
+        when (@content.count / 8 + 1)..(@content.count / 7) then 'nm'
         else 'big'
         end
 
